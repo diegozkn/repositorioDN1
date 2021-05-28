4a778448f7df6e9bd2f5e672e6c91e3c4ff1c7cc
diegozkn <diego.nahuel.93@gmail.com>
2021-05-28 13:49:19 -0400

    se agrega la clase de  operaciones, asignacion comparacion aritmeticos y logicos

--- asignaciondedatos_operaciones.R
+++ asignaciondedatos_operaciones.R
@@ -1,3 +1,9 @@
+
+
+###############################################################
+################### CREACIÓN DE VARIABLES #####################
+###############################################################
+###############################################################
 # creando la variable de nombre hola
 
 hola <- "pastito"
@@ -7,3 +13,103 @@ variable1 <- "hola"
 12312354423
 c3po
 r2d2
+
+#usar ctrl + enter para ejecutar el comando
+
+d1 <- "hola"
+
+#el nombre que le doy a la variable debe ser alfanumerico no puede ser
+#numero solamente.
+
+c3po <- "hombre de lata"
+r2d2 <- "el silvador"
+
+resultadodesuma <- 5+10
+elboleano <- T
+otronumero <- 3.13543
+
+###############################################################
+################### OPERADORES #####################
+###############################################################
+###############################################################
+
+###asignacion
+
+variable2 <- "hoy es buen dia para jugar CS"
+
+#puede ser texto, entero, reales, booleanos, etc.
+
+###comparación
+#solo me dan respuestas logicas o booleanas
+
+4<3
+
+###operaciones logicas
+#aquellas que operan con valores logicos
+
+
+#AND (agregacion)
+
+T && T
+1 && 0
+
+#OR (Exclusion)
+
+T || T
+T || F
+1 || 4
+
+#NOT (Negacion)
+
+!T #FALSO EN NEGACION
+!F #VERDADERO EN NEGACION
+
+#ejemplos operaciones logicas
+
+a <- 10
+b <- 9
+c <- 8
+
+(a > b) || (b < c)
+
+(a < b) && (b > c)
+
+variable1 <- "hola mundo"
+variable1 <- variable1 =="hola mundo"
+
+variable1 <- 5 != 5
+
+###operadores Aritmeticos
+
+#suma 
+
+5+7
+
+#multiplicacion
+
+3*10
+
+#division
+
+18/6
+
+#modulo
+#obtencion del resto en una divicion
+
+2%%2
+4%%3
+
+#como saber si un numero es par o impar
+
+32%%2
+53%%2
+5%%2
+34%%2
+6%%2
+43%%2
+# los que son pares dan mod = 0 , los que son impares dan mod=1
+
+a <- 4
+
+a %% 2 == 0
+
