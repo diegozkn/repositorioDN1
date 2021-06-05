e1314f37e5642b0480a61d5fb3e2e05e9a2b08ac
diegozkn <diego.nahuel.93@gmail.com>
2021-06-05 03:02:40 -0400

    clase 28

--- asignaciondedatos_operaciones.R
+++ asignaciondedatos_operaciones.R
@@ -148,3 +148,68 @@ if((edadinicial <= edadpersona) && (edadfinal >= edadpersona)){ print("la person
 edadpersona <- 30
 if((edadinicial <= edadpersona) && (edadfinal >= edadpersona)){ print("la persona es millenial")}
 
+########RANGO ETARIO##############
+
+anioNacimiento<- 1998
+print(anioNacimiento)
+
+#v1 .- si el año de nacimiento es baby boom
+#v2 -. si la persona nacio entre x años
+
+#condiciones que determinan que una persona no es baby boom
+#condicion: persona nacion antes del 1946
+
+anioNacimiento <- 1946
+
+#condicion: persona nacio despues de 1964
+anioNacimiento > 1964
+
+#v3-. si la persona nacio antes del 1946 y despues de 1964, no es baby boom
+anioNacimiento < 1946 || anioNacimiento > 1964
+
+
+#condicionales
+
+if(anioNacimiento < 1946 || anioNacimiento > 1964){
+  print("La persona no es baby boom")
+}
+
+
+#probando con otro año
+
+anioNacimiento <-1950
+if(anioNacimiento < 1946 || anioNacimiento > 1964){
+  print("La persona no es Baby boom")
+}
+
+anioNacimiento <-1950
+if(anioNacimiento < 1946 || anioNacimiento > 1964){
+  print("La persona no es Baby boom")
+}else(
+  print("esta persona si es baby boom")
+)
+
+
+###### correccion propia a la pregunta 
+
+#mi condicion cuando una persona es baby boom
+#si cuando su anio de nacimiento es mayor a 1946 y menor a 1964
+anioNacimiento <-1950
+anioNacimiento >= 1946 && anioNacimiento <=1964
+
+#con condicional if -else
+
+if(anioNacimiento >= 1946 && anioNacimiento <= 1964){
+  print("es baby boom")
+}else{
+  print("no es baby boom")
+}
+
+#preguntando si una persona es baby boom, generacion silenciosa o una desconocida
+#pregunta que determina si una persona es de la generacion silenciosa (1928-1945)
+#si la persona nacio despues de 1927 y antes de 1946
+
+anioNacimiento>=1928 && anioNacimiento<=1945
+
+#con condicional if - else
+
